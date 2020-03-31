40c880db6f7bbff497251ae442950546ed1b5101
Mauricio Nuñez <mauronunezmolina@gmail.com>
2020-03-30 21:32:40 -0300

    Enable /token as unauth path

--- src/main/java/com/github/mauronunez/course/restapi/RestapiApplication.java
+++ src/main/java/com/github/mauronunez/course/restapi/RestapiApplication.java
@@ -1,13 +1,35 @@
 package com.github.mauronunez.course.restapi;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.github.mauronunez.course.restapi.security.JWTAuthorizationFilter;
 
 @SpringBootApplication
 public class RestapiApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(RestapiApplication.class, args);
+	}
+	
+	@EnableWebSecurity
+	@Configuration
+	class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable()
+				.authorizeRequests()
+				.antMatchers(HttpMethod.GET, "/token").permitAll()
+				.anyRequest().authenticated();
+		}
 	}
 
 }
