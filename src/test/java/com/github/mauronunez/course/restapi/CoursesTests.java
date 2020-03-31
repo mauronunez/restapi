87c6f97f18188c2707a1486dc7e6e857a79e57e8
Mauricio Nuñez <mauronunezmolina@gmail.com>
2020-03-31 17:58:38 -0300

    Remove unused imports

--- src/test/java/com/github/mauronunez/course/restapi/CoursesTests.java
+++ src/test/java/com/github/mauronunez/course/restapi/CoursesTests.java
@@ -7,15 +7,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-
-import com.github.mauronunez.course.restapi.controller.CourseController;
-import com.github.mauronunez.course.restapi.controller.TokenController;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
